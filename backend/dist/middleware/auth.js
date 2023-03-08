7054b16affd0bdc8950e1606d19438e97a5387da
S. M Shawal Chowdhury <shafayet.shawal@gmail.com>
2023-03-08 16:47:39 +0600

    console log to see req.session

--- backend/dist/middleware/auth.js
+++ backend/dist/middleware/auth.js
@@ -6,6 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.requiresAuth = void 0;
 const http_errors_1 = __importDefault(require("http-errors"));
 const requiresAuth = (req, res, next) => {
+    console.log(req.session);
     if (req.session.userId) {
         next();
     }
